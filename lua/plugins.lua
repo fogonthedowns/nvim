dc2fe522aee56d4fcb769fc96515e26dad98c826
Justin Zollars <jzollars@goshippo.com>
2022-04-29 09:14:45 -0700

    initial commit

---
+++ lua/plugins.lua
@@ -0,0 +1,14 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+require'lspconfig'.gopls.setup{} -- gopls language server (LSP) https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+
+return require('packer').startup(function()
+  -- to install package. Import library with use ''. Open nvim and enter :PackerUpdate
+  use 'wbthomason/packer.nvim' -- Package manager
+  use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
+  use 'ludovicchabant/vim-gutentags' -- cTags
+end)
+

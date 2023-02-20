df9c019abb50b5153c9c5aa9ce24f89fb4eb0312
Justin Zollars <142996+fogonthedowns@users.noreply.github.com>
2023-02-19 19:14:09 -0500

    update

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -44,3 +44,12 @@ vim.api.nvim_create_autocmd('LspAttach', {
     bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
   end
 })
+
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+end
+require('lspconfig').gopls.setup({
+   on_attach = on_attach,
+   autostart = true
+})

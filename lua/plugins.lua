25acc04b937ff2430cc78795d1829a2877c53c57
Justin Zollars <142996+fogonthedowns@users.noreply.github.com>
2023-02-28 13:50:31 -0500

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,8 +12,6 @@ require'lspconfig'.gopls.setup{ -- gopls language server (LSP) https://github.co
   vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
   vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
   vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, {buffer=0})
-
-
 end
 }
 -- connect to server
@@ -59,5 +57,35 @@ use {
       }
     end
   }
+
+  use {
+    "lewis6991/hover.nvim",
+    config = function()
+        require("hover").setup {
+            init = function()
+                -- Require providers
+                require("hover.providers.lsp")
+                -- require('hover.providers.gh')
+                -- require('hover.providers.gh_user')
+                -- require('hover.providers.jira')
+                -- require('hover.providers.man')
+                -- require('hover.providers.dictionary')
+            end,
+            preview_opts = {
+                border = nil
+            },
+            -- Whether the contents of a currently open hover window should be moved
+            -- to a :h preview-window when pressing the hover keymap.
+            preview_window = false,
+            title = true
+        }
+
+        -- Setup keymaps
+        vim.keymap.set("n", "K", require("hover").hover, {desc = "hover.nvim"})
+        vim.keymap.set("n", "gK", require("hover").hover_select, {desc = "hover.nvim (select)"})
+    end
+}
+
+
 end)
 

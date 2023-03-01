f7d6b81d1030fe5c2e83da8ecd614b546b8c2a81
Justin Zollars <jzollars@experientgroup.com>
2023-03-01 13:49:42 -0500

    color updates

--- init.vim
+++ init.vim
@@ -1,5 +1,6 @@
 lua require('plugins')
 lua require('copilot')
 lua require('lsp')
+lua require('colorscheme')
 set number
 au BufWritePost *.go !gofmt -w %
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -39,12 +39,19 @@ use {
   requires = { {'nvim-lua/plenary.nvim'} }
 }
 
+  -- Colorschemes
+  use "folke/tokyonight.nvim"
+  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
+  use "lunarvim/darkplus.nvim"
+
   local builtin = require('telescope.builtin')
   vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
   vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
   vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
   vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
   vim.keymap.set('n', '<leader>gr', builtin.lsp_references, {})
+  vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, {})
+  vim.keymap.set('n', '<leader>ws', builtin.lsp_dynamic_workspace_symbols, {})
   -- use 'folke/lsp-colors.nvim' -- https://github.com/folke/lsp-colors.nvim
   use { -- https://github.com/folke/trouble.nvim
     "folke/trouble.nvim",

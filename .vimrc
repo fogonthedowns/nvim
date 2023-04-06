ec23a6e4a0e966a32efda64f0ffa07cb1310e234
Justin Zollars <142996+fogonthedowns@users.noreply.github.com>
2023-04-06 11:30:08 -0400

    update

--- .vimrc
+++
@@ -1,3 +0,0 @@
-set encoding=utf8
-let g:airline_powerline_fonts = 1
-au BufWritePost *.go !gofmt -w %
--- init.lua
+++ init.lua
@@ -10,4 +10,31 @@ require('jz.lualine')
 require('jz.nvim-cmp')
 require("jz.lsp_config")
 require("jz.dap")
+require("jz.telescope")
+
+vim.o.encoding = 'utf-8'
+
+-- Enable Powerline fonts in the Airline plugin
+vim.g.airline_powerline_fonts = 1
+
+-- Automatically format Go files using gofmt
+vim.api.nvim_exec([[
+  augroup auto_fmt
+    autocmd!
+    autocmd BufWritePost *.go !gofmt -w %
+  augroup END
+]], false)
+
+-- Highlight search results in the current buffer
+vim.o.hlsearch = true
+
+-- Update search results as you type
+vim.o.incsearch = true
+
+-- Disable case-insensitive searching
+vim.o.ignorecase = false
+vim.o.smartcase = true
+
+-- Clear the search register
+vim.cmd('let @/ = ""')
 

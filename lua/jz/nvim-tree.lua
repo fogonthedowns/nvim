d0ebe8a8d516a069610017edcf6539e3d67c6794
Justin Zollars <jzollars@experientgroup.com>
2023-08-16 14:10:18 -0400

    followed https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup

--- lua/jz/nvim-tree.lua
+++ lua/jz/nvim-tree.lua
@@ -24,6 +24,25 @@ nvimtree.setup({
   },
   filters = {
     dotfiles = true,
-  },
-  open_on_setup = true
+  }
+--  open_on_setup = true
 })
+
+local function open_nvim_tree(data)
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+-- open on setup nvim-tree
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })

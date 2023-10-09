c9c0350d315d733ab5228d4128295939677e1c8c
Justin Zollars <jzollars@experientgroup.com>
2023-10-09 13:37:47 -0400

    adds k8 schema

--- lua/jz/yamlls.lua
+++ lua/jz/yamlls.lua
@@ -5,7 +5,10 @@ require('lspconfig')['yamlls'].setup {
   settings = {
     yaml = {
       schemas = {
-        ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.0/schema.yaml"] = "/*"
+        ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0/deployment.json"] = "/*-deployment.yaml",
+        ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0/service.json"] = "/*-service.yaml",
+        ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0/configmap.json"] = "/*-configmap.yaml",
+        ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.0/schema.yaml"] = "/*-openapi.yaml"
       }
     }
   }
